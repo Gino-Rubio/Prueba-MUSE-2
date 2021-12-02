1b9e62c6181cd65c826b05cb91b4ce814a50498c
Gino-Rubio <higinio.rubio@alumnos.upm.es>
2021-12-02 12:10:25 +0100

    Agregar archivos de proyecto.

---
+++ Prueba MUSE 2/Prueba MUSE 2.f90
@@ -0,0 +1,25 @@
+!  PruebaMUSE2.f90 
+!
+!  FUNCTIONS:
+!  PruebaMUSE2 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: PruebaMUSE2
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program PruebaMUSE2
+
+    implicit none
+
+    ! Variables
+
+    ! Body of PruebaMUSE2
+    print *, 'Hello World'
+
+    end program PruebaMUSE2
+
